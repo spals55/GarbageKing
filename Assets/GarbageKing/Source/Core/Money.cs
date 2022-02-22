704004419ced47cba3a512fd1a05a2bf8cf1bfde
Daniil Potapov <teeky@bk.ru>
2022-02-23 02:21:53 +0500

    Folder managment

--- Assets/GarbageKing/Source/Core/Money.cs
+++ Assets/GarbageKing/Source/Core/Money.cs
@@ -5,10 +5,15 @@ using DG.Tweening;
 
 public class Money : MonoBehaviour
 {
+    [SerializeField] private int _amount;
+
+    public int Amount => _amount;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.TryGetComponent(out ICharacter character))
         {
+            //На первое время.
             transform.DOMove(character.transform.position, 0.2f);
             Destroy(gameObject, 0.2f);
         }
