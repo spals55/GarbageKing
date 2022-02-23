4faa1cdc4ee37893e6ce450d7c559e6f9449909a
Daniil Potapov <teeky@bk.ru>
2022-02-23 16:11:30 +0500

    Folder refactoring

--- Assets/GarbageKing/Source/Core/GarbageBag.cs
+++ Assets/GarbageKing/Source/Core/GarbageBag.cs
@@ -1,14 +1,21 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class GarbageBag : MonoBehaviour
 {
+    private List<ITrash> _trash = new List<ITrash>();
+
     public void Add(ITrash trash)
     {
         trash.Hide();
+        trash.transform.parent = transform;
+        _trash.Add(trash);
     }
 
     public void Remove(ITrash trash)
     {
         trash.Show();
+        trash.transform.parent = null;
+        _trash.Remove(trash);
     }
 }
